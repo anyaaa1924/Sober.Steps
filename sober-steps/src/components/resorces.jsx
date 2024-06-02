c31aab3a117274715081dcba52b580898bc4dc17
Ananya Bhat <135028764+anyaaa1924@users.noreply.github.com>
2024-06-02 11:52:47 +0530

    front

--- sober-steps/src/components/resorces.jsx
+++ sober-steps/src/components/resorces.jsx
@@ -1,30 +1,37 @@
 import React from 'react';
+import { FaNewspaper, FaVideo, FaStickyNote, FaFileAlt, FaImage, FaMicrophone } from 'react-icons/fa'; // Import relevant icons
 import './resources.css';
 
 const ResourcesPage = () => (
   <main className="resources main-content">
     <div className="resource-types">
       <div className="resource-type articles">
+        <FaNewspaper className="resource-icon" />
         <h3>Articles</h3>
         <button className="resource-button" onClick={() => window.open("https://www.aa.org", "_blank")}>Take Me There</button>
       </div>
       <div className="resource-type videos">
+        <FaVideo className="resource-icon" />
         <h3>Videos</h3>
         <button className="resource-button" onClick={() => window.open("https://www.aa.org", "_blank")}>Take Me There</button>
       </div>
       <div className="resource-type notes">
+        <FaStickyNote className="resource-icon" />
         <h3>Notes</h3>
         <button className="resource-button" onClick={() => window.open("https://www.aa.org", "_blank")}>Take Me There</button>
       </div>
       <div className="resource-type worksheets">
+        <FaFileAlt className="resource-icon" />
         <h3>Worksheets</h3>
         <button className="resource-button" onClick={() => window.open("https://www.aa.org", "_blank")}>Take Me There</button>
       </div>
       <div className="resource-type photos">
+        <FaImage className="resource-icon" />
         <h3>Photos</h3>
         <button className="resource-button" onClick={() => window.open("https://www.aa.org", "_blank")}>Take Me There</button>
       </div>
       <div className="resource-type voice-recordings">
+        <FaMicrophone className="resource-icon" />
         <h3>Voice Recordings</h3>
         <button className="resource-button" onClick={() => window.open("https://www.aa.org", "_blank")}>Take Me There</button>
       </div>
